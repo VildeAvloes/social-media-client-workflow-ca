6ca0cb025e4fe38f0444acf796e93ae892e3a05b
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2024-02-15 10:12:00 +0100

    Added a unit test for logout function and updated the mock file

--- src/js/storage/localStorage.mock.js
+++ src/js/storage/localStorage.mock.js
@@ -2,4 +2,7 @@ export default {
   setItem: jest.fn((key, value) => (localStorage[key] = value)),
   getItem: jest.fn((key) => localStorage[key] || null),
   removeItem: jest.fn((key) => delete localStorage[key]),
+  clear: jest.fn(function () {
+    Object.keys(this).forEach((key) => delete this[key]);
+  }),
 };
